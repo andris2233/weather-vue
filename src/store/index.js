9791ed0606f56cc63d32a43c84e00c27fb883a3d
Andris Tsildermanis <tsildermanis@o-code.ru>
2020-10-19 09:35:30 +0400

    added store methods to work with localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -5,15 +5,42 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    key: 'c3f70fc04c5deaafe53df4a379f3a654',
+    favorites: JSON.parse(localStorage.getItem('favoritePlaces') || '[]'),
+    maxLength: 5,
   },
   mutations: {
+    setFavorite(state, place) {
+      const { favorites, maxLength } = state;
+      if (favorites.length < maxLength) {
+        state.favorites.push(place);
+        localStorage.setItem('favoritePlaces', JSON.stringify(state.favorites));
+      }
+    },
+    removeFavorite(state, index) {
+      if (index) {
+        state.favorites.splice(index, 1);
+        localStorage.setItem('favoritePlaces', JSON.stringify(state.favorites));
+      }
+    },
   },
   actions: {
+    setFavorite({ commit }, place) {
+      commit('setFavorite', place);
+    },
+    removeFavorite({ commit, favorites }, place) {
+      const index = favorites.findIndex((item) => item === place);
+      commit('removeFavorite', index);
+    },
   },
   getters: {
-    getKey(state) {
-      return state.key;
+    getFirstFavorite({ favorites }) {
+      return favorites.length ? favorites[0] : '';
+    },
+    canSet({ favorites, maxLength }) {
+      return favorites.length < maxLength;
+    },
+    isFavorite({ favorites }) {
+      return (place) => favorites.includes(place);
     },
   },
 });
